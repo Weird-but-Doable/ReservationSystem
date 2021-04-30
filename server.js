3c4029e7de96ca0212adaae0c4b6fd73966f457a
Kemal Demirgil <kemal.demirgil@hotmail.com>
2021-04-29 21:30:59 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -8,26 +8,52 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'home.html')));
-app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, 'tables.html')));
-app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, 'reserve.html')));
+app.get('/tables', (req, res) => res.sendFile(path.join(__dirname, '/tables.html')));
+app.get('/reserve', (req, res) => res.sendFile(path.join(__dirname, '/reserve.html')));
+
+app.get('/api/tables', (req, res) => res.json(reservation));
+app.get('/api/waitlist', (req, res) => res.json(waitList));
+
+
+const reservation = [
+    {
+        customerID: "123",
+        customerName: "kemal",
+        customerEmail: "kemal@asd",
+        phoneNumber: "6132321213"
+    }
+]
+
+const waitList = [
+    {
+        customerID: "2223",
+        customerName: "tatyana",
+        customerEmail: "kasdasdsasdl@asd",
+        phoneNumber: "613232231123211231231321213"
+    }
+]
 
 
 
+app.post('/api/tables', (req, res) => {
+    const newReservation = req.body;
+
+    if (reservation.length >= 5) {
+        app.post('/api/waitlist', (req, res) => {
+            console.log(newReservation);
+          
+            waitList.push(newReservation);
+            res.json(newReservation);
+        });
+    } else {
+        console.log(newReservation);
+      
+        reservation.push(newReservation);
+        res.json(newReservation);
+    }
+});
 
 
-// app.post('/api/characters', (req, res) => {
-//     // req.body hosts is equal to the JSON post sent from the user
-//     // This works because of our body parsing middleware
-//     const newCharacter = req.body;
-  
-//     // Using a RegEx Pattern to remove spaces from newCharacter
-//     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//     newCharacter.routeName = newCharacter.name.replace(/\s+/g, '').toLowerCase();
-//     console.log(newCharacter);
-  
-//     characters.push(newCharacter);
-//     res.json(newCharacter);
-//   });
 
 
 
